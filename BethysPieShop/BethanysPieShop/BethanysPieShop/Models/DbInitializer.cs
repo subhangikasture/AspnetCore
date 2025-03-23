bfad96e8b155aa68b86efcee03b438162d667094
Admin <subhangikasture@gmail.com>
2025-03-23 22:42:28 +0530

    ER : some error I encountered

---
+++ BethysPieShop/BethanysPieShop/BethanysPieShop/Models/DbInitializer.cs
@@ -0,0 +1,63 @@
+﻿namespace BethanysPieShop.Models
+{
+    public class DbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            BethanysPieShopDbContext context =
+                applicationBuilder.ApplicationServices.GetRequiredService<BethanysPieShopDbContext>();
+
+            if (!context.Categories.Any())
+            {
+                context.Categories.AddRange(Categories.Select(c => c.Value));
+            }
+
+            if (!context.Pies.Any())
+            {
+                context.AddRange
+                    (
+                    new Pie
+                    {
+                        Name = "Strawberry Pie",
+                        Price = 15.95M,
+                        ShortDescription = "Our famous strawberry pie!",
+                        LongDescription = "Icing carrot cake jelly beans. Sweet roll candy canes fruitcake. Tiramisu candy canes"
+                    },
+                    new Pie
+                    {
+                        Name = "Cheese Cake",
+                        Price = 18.95M,
+                        ShortDescription = "Plain cheese cake. Plain pleasure.",
+                        LongDescription = "Icing carrot cake jelly beans. Sweet roll candy canes fruitcake. Tiramisu candy canes"
+                    }
+                    );
+            }
+
+            context.SaveChanges();
+        }
+
+        private static Dictionary<string, Category>? categories;
+
+        public static Dictionary<string, Category> Categories
+        {
+            get
+            {
+                if (categories == null)
+                {
+                    var categoriesList = new Category[]
+                    {
+                            new Category { CategoryName = "Fruit Pies", CategoryDescription = "All-fruity pies" },
+                            new Category { CategoryName = "Cheese Cakes", CategoryDescription = "Cheesy all the way" },
+                            new Category { CategoryName = "Seasonal Pies", CategoryDescription = "Get in the mood for a seasonal pie" }
+                    };
+                    categories = new Dictionary<string, Category>();
+                    foreach (Category category in categoriesList)
+                    {
+                        categories.Add(category.CategoryName, category);
+                    }
+                }
+                return categories;
+            }
+        }
+    }
+}
--- BethysPieShop/BethanysPieShop/BethanysPieShop/Program.cs
+++ BethysPieShop/BethanysPieShop/BethanysPieShop/Program.cs
@@ -1,11 +1,16 @@
+using System;
 using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews(); /// Add MVC services
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>(); // Add scoped service for ICategoryRepository
-builder.Services.AddScoped<IPieRepository, MockPieRepository>(); // Add scoped service for IPieRepository
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>(); // Add scoped service for ICategoryRepository
+builder.Services.AddScoped<IPieRepository, PieRepository>(); // Add scoped service for IPieRepository
 
+// Configure DbContext with connection string
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 app.UseStaticFiles();   //Use static files e.g. css, js, images
@@ -15,6 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage(); //Use developer exception page when working on development environment
 }
 
-app.MapDefaultControllerRoute();  // Map default controller route
+app.MapDefaultControllerRoute();  // Map default controller route => "{Controller = Home}/{action = Index}/{id?}"
 
+DbInitializer.Seed(app); // Seed the database
 app.Run();
