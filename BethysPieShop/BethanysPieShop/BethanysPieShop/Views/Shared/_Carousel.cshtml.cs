d02b84b64e7bfcea0f743abce3a60f0d59aca45f
Admin <subhangikasture@gmail.com>
2025-03-31 16:36:04 +0530

    Adding a partial _PieCard and Carousel views

---
+++ BethysPieShop/BethanysPieShop/BethanysPieShop/Views/Shared/_Carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BethanysPieShop.Views.Shared
+{
+    public class _CarouselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
