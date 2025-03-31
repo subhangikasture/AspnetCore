3ae7ff70847eb29979f3d8b84d71df16c887863b
Admin <subhangikasture@gmail.com>
2025-03-31 07:16:14 +0530

    Added Home controller

---
+++ BethysPieShop/BethanysPieShop/BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
