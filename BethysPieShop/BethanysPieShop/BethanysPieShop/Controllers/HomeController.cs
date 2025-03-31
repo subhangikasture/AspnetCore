70f6554c7f4442dc6b933f9fe482765847f790b8
Admin <subhangikasture@gmail.com>
2025-03-31 10:46:33 +0530

    Added home controller with Modified dbInitializer , added index.html to the default landing page of the BethysPiShop website

--- BethysPieShop/BethanysPieShop/BethanysPieShop/Controllers/HomeController.cs
+++ BethysPieShop/BethanysPieShop/BethanysPieShop/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            var piesOfTheWeek = _pieRepository.PiesOfTheWeek;
+            var homeViewModel = new HomeViewModel(piesOfTheWeek);
+            return View(homeViewModel);
         }
     }
 }
---
+++ BethysPieShop/BethanysPieShop/BethanysPieShop/Models/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BethanysPieShop.Models
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+        public HomeViewModel(IEnumerable<Pie> piesOfTheWeek)
+        {
+            PiesOfTheWeek = piesOfTheWeek;
+        }
+
+    }
+}
