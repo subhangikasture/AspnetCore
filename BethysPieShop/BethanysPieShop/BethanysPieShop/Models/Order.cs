a61b8d907c8626293cf155295b97558873f45ada
Admin <subhangikasture@gmail.com>
2025-04-05 15:18:50 +0530

    Added Server side validation

--- BethysPieShop/BethanysPieShop/BethanysPieShop/Models/Order.cs
+++ BethysPieShop/BethanysPieShop/BethanysPieShop/Models/Order.cs
@@ -1,18 +1,39 @@
-﻿namespace BethanysPieShop.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BethanysPieShop.Models
 {
     public class Order
     {
+        [BindNever]
         public int OrderId { get; set; }
         public List<OrderDetail>? OrderDetails { get; set; }
+
+        [Required(ErrorMessage = "Please enter your first name.")]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name.")]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your address.")]
+        [Display(Name = "Address Line 1")]
+        [StringLength(100, ErrorMessage = "Address line 1 cannot be longer than 100 characters.")]
         public string AddressLine1 { get; set; }
+
         public string AddressLine2 { get; set; }
         public string ZipCode { get; set; } = string.Empty;
         public string City { get; set; } = string.Empty;
         public string? State { get; set; } //nullable
         public string Country { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please enter your email address.")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email address.")]
         public string Email { get; set; } = string.Empty;
         public decimal OrderTotal { get; set; }
         public DateTime OrderPlaced { get; set; }
