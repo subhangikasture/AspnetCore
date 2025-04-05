8f890f9e4bd1b76f5ebb9be975ffd2cc5376ec94
Admin <subhangikasture@gmail.com>
2025-04-05 14:55:39 +0530

    Added Order controller and Checkout complete view

--- BethysPieShop/BethanysPieShop/BethanysPieShop/Controllers/OrderController.cs
+++ BethysPieShop/BethanysPieShop/BethanysPieShop/Controllers/OrderController.cs
@@ -12,8 +12,32 @@ namespace BethanysPieShop.Controllers
             _orderRepository = orderRepository;
             _shoppingCart = shoppingCart;
         }
-        public IActionResult Checkout()
+        public IActionResult Checkout() //GET defualt
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        { 
+            var items  = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            if(_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("","Your cart is empty, add some pies first");
+            }
+            if (ModelState.IsValid) 
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+
+        public IActionResult CheckoutComplete()
+        {
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. you will soon enjoy our delicious pies!";
             return View();
         }
     }
