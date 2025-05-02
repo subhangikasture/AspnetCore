d61d16e1709acb660bbfa69d8dd05df02cada641
Subhangi <subhangikasture@gmail.com>
2025-05-02 19:52:47 +0530

    Demo web api

---
+++ DemoWebApi/DemoWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using DemoWebApi.Model;
+using DemoWebApi.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmplolyeeBuisenesslogic employee;
+
+        public EmployeeController(IEmplolyeeBuisenesslogic employee)
+        {
+            this.employee = employee;
+        }
+
+        [HttpGet("Get")]
+        
+        public List<Employee> Get()
+        {
+           return employee.GetEmployeeDetails();       
+        }
+
+        [HttpPost("Post")]
+        public void Post(Employee employeeData)
+        {
+            employee.AddEmployeeDetails(employeeData);
+        }
+    }
+}
---
+++ DemoWebApi/DemoWebApi/Repository/IEmplolyeeBuisenesslogic.cs
@@ -0,0 +1,11 @@
+﻿using DemoWebApi.Model;
+
+namespace DemoWebApi.Repository
+{
+    public interface IEmplolyeeBuisenesslogic
+    {
+        public List<Employee> GetEmployeeDetails();
+
+        public void AddEmployeeDetails(Employee employee);
+    }
+}
