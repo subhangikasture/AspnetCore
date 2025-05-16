1c1b4e39d9f58ac5f79a0ce615b965fa33345413
Sonalidev-star <sonu1998dev@gmail.com>
2025-05-16 20:09:08 +0530

    Singleton pattern

---
+++ Design Patterns/Singleton/Singleton/Singleton/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Logger
+    {
+        private Logger() {
+            Console.WriteLine("I am a private constructor");
+        }
+
+        private static Logger _loggerInstance;
+
+        public static Logger getLoggerInstance
+        {
+            get
+            {
+                if (_loggerInstance == null)
+                {
+                    Console.WriteLine("Instance creation");
+                    _loggerInstance = new Logger();
+                }
+                return _loggerInstance;
+            }
+        }
+
+        public void Log(string message)
+        { 
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Design Patterns/Singleton/Singleton/Singleton/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Logger.getLoggerInstance.Log("hello");
+Logger.getLoggerInstance.Log("HELLO2");
+
+Console.ReadLine();
